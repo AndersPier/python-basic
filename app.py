8fd1f3830ed7c09424634dda4908679664199a06
Anders Pier <anders@pier.dk>
2022-04-27 17:38:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! New - 27-04"
+    return "Hello World! New - 27-04-2022"
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
